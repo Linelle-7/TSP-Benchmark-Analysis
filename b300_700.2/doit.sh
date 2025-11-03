9d3326c7aeb87dec152eea66febd188e82eb5809
Linelle-7 <linelle.meneckdem@smail.inf.h-brs.de>
2025-11-03 19:43:05 +0100

    Datensatzt zur Evaluierung der Log-Linear Regression hinzugefügt

---
+++ b300_700.2/doit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+j=0
+while [ $j -lt 2000 ]; do
+    tspstart=`date +%d.%m.%Y-%H:%M:%S`
+    tsp -i b300_700.2_${j}.json heuristic greedy | \
+    tsp improve 2opt | \
+    tsp -o b300_700.2_${j}_imp.json improve locenum -w 200 -s 100
+    tspimprove=`date +%d.%m.%Y-%H:%M:%S`
+    tsp -i b300_700.2_${j}_imp.json -o b300_700.2_${j}_sol.json solve bac
+    tspend=`date +%d.%m.%Y-%H:%M:%S`
+    echo $j $tspstart $tspimprove $tspend >> runtimes
+    j=$[j+1]
+done
---
+++ b700_1000.2/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+j=0
+while [ $j -lt 2000 ]; do
+    n=`echo $((RANDOM%300+700))`
+    tsp -o b700_1000.2_${j}.json generate -n $n
+    j=$[j+1]
+done
